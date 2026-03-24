605a4a6a711120fc1ba8730b15b06f3c0b5e320e
Wilson Santos <wilson.saantos@hotmail.com>
2026-03-24 17:56:57 -0300

    fix: build

--- src/modules/profile/profile.module.ts
+++ src/modules/profile/profile.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service.js';
 import { ProfileController } from './profile.controller.js';
+import { S3Service } from '../../infra/s3/s3.service.js';
 
 @Module({
-  providers: [ProfileService],
-  controllers: [ProfileController]
+  providers: [ProfileService, S3Service],
+  controllers: [ProfileController],
 })
 export class ProfileModule {}
