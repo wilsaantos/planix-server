203d658b22862aaac5ea91093f707d604ca85ed5
Wilson Santos <wilson.saantos@hotmail.com>
2026-03-24 17:52:15 -0300

    feat: s3 connection and profile image logic

--- src/modules/profile/profile.service.spec.ts
+++ src/modules/profile/profile.service.spec.ts
@@ -1,8 +1,8 @@
 import { jest } from '@jest/globals';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProfileService } from './profile.service.js';
-import { S3Service } from '../../infra/s3/s3.service';
-import { PrismaService } from '../../infra/prisma/prisma.service';
+import { S3Service } from '../../infra/s3/s3.service.js';
+import { PrismaService } from '../../infra/prisma/prisma.service.js';
 
 describe('ProfileService', () => {
   let service: ProfileService;
