5b8237e6f3185a945de7d5b2921c0a6e13d714a7
Wilson Santos <wilson.saantos@hotmail.com>
2026-03-25 09:20:54 -0300

    fix: docker prisma migrations and jwt guard

--- src/common/guards/jwt-auth/jwt-auth.guard.ts
+++ src/common/guards/jwt-auth/jwt-auth.guard.ts
@@ -1,9 +1,5 @@
-import { CanActivate, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') implements CanActivate {
-  canActivate(): boolean {
-    return true;
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
